eb4872cce66c790bc191747c54c8c3b00226a19e
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-08 01:43:48 -0500

    used for loop and push method to solve this problem

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function fibonacciGenerator(n){
+// counter
+// s1
+// s2
+// output = []
 
+
+// n = 1 output 0
+// n = 2 output 1
+
+
+
+
+function fibonacciGenerator(n){
+    var output = [];
+    if (n === 1){
+        output = [0];
+    } else if (n == 2){
+        output = [0,1]
+    } else {
+        output = [0,1];
+        for (var i = 2; i < n; i++){
+            output.push(output[output.length -2] + output[output.length -1]);
+        }
+    }
+
+    return output
 }
 
+var result = fibonacciGenerator(10)
 
-
-
+console.log(result)
